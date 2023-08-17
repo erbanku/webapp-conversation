a185934694f1ac6d3fca6f313163910b222f41f0
Cactus <erbanku@gmail.com>
2023-08-17 09:32:45 +0800

    💫 Regular Updates

--- config/index.ts
+++ config/index.ts
@@ -1,13 +1,14 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+// APP_ID and API_KEY
+export const APP_ID = 'fb185519-4a14-4c33-9f12-c42e01d98759'
+export const API_KEY = 'app-GvvTqWL8pNUN5rtH4SwWIkSW'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'zh-Hans',
+    title: 'Chat APP',
+    description: '',
+    copyright: '',
+    privacy_policy: '',
+    default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
